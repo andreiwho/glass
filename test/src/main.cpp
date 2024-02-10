293fe2c6b55987c079c55d66916d5de4cbc3d5f8
andreiwho <protsenko.ro.c@gmail.com>
2024-02-10 21:48:41 +0100

    Testing of the GLFW events

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,12 +1,17 @@
 #include "glass/glass.h"
 #include "assert.h"
+#include "print"
 
 namespace gp = glass::platform;
+
+void onWindowEvent(const gp::WindowEvent& event) {
+    std::println("{}", event.toString());
+}
 
 int main() {
     assert(gp::init());
 
-    gp::Window* window = gp::createWindow({ 1280, 720, "Hello GLass", nullptr });
+    gp::Window* window = gp::createWindow({ { 1280, 720 }, "Hello Glass", nullptr, onWindowEvent });
     while (gp::isMainWindowAlive()) {
         gp::pollEvents();
     }
